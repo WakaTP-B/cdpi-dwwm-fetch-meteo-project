7c9509096736a58ae8ae5694acef09cd75b73417
WakaTP-B <courcelles.amaury@orange.fr>
2025-10-14 01:26:48 +0200

    commit

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
 function main() {
+    navigator.geolocation.getCurrentPosition(onPosition);
 
     const search = document.querySelector('.search-bar');
-    const cityInput = document.querySelector('.city');
+    const cityInput = document.querySelector('.cityInput');
 
     if (search) {
         search.addEventListener('submit', e => {
             e.preventDefault();
-
+            const cityInputValue = cityInput.value.toLowerCase();
+            searchCity(cityInputValue)
         });
     }
 }
 main();
 
-function searchCity (){
-    
+function searchCity(cityInputValue) {
+    fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${cityInputValue}&count=1&language=fr&format=json`)
+        .then(response_obj => response_obj.json())
+        .then(city => {
+            console.log(city.results)
+        })
 }
+function weather(lat, long) {
+    fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,weather_code,precipitation&current=is_day,temperature_2m,precipitation,weather_code`)
+        .then(response_obj => response_obj.json())
+        .then(weather => {
 
+        })
+}
 
 
 /**
@@ -24,5 +36,12 @@ function searchCity (){
  * @param {*} position_obj 
  */
 function onPosition(position_obj) {
+
+    const latUser = position_obj.coords.latitude;
+    const longUser = position_obj.coords.longitude;
+
+}
+
+function weatherOnPosition() {
 
 }
